77a0ea94cf06659c1e855691ef77d071a3fa4e97
Richard Shaw <richardmorganshaw@gmail.com>
2021-01-21 14:45:25 -0800

    Added adding and editing functionality

--- _richard/client/src/App.js
+++ _richard/client/src/App.js
@@ -1,11 +1,5 @@
 import AddFood from './pages/AddFood'
 
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch
-} from 'react-router-dom'
-
 const App = () => {
   return <AddFood />
 }
--- _richard/client/src/pages/AddFood/AddFood.js
+++ _richard/client/src/pages/AddFood/AddFood.js
@@ -7,7 +7,14 @@ import Button from '@material-ui/core/Button'
 import TextField from '@material-ui/core/TextField'
 import FoodAPI from '../../utils/FoodAPI'
 import CatagoryAPI from '../../utils/CatagoryAPI'
-import { InputLabel, MenuItem, Select, Typography } from '@material-ui/core'
+import {
+  Grid,
+  InputLabel,
+  MenuItem,
+  Select,
+  Typography,
+  CardActionArea,
+} from '@material-ui/core'
 
 import numberToMoney from '../../utils/lib/numberToMoney'
 
@@ -23,9 +30,12 @@ const useStyles = makeStyles((theme) => ({
   media: {
     height: 280,
   },
+  foodCard: {
+    width: '100%',
+  },
 }))
 
-const { createFood } = FoodAPI
+const { createFood, getFood, updateFood, deleteFood } = FoodAPI
 const { createCatagory, getCatagories, deleteCatagory } = CatagoryAPI
 
 const AddFood = (props) => {
@@ -38,10 +48,10 @@ const AddFood = (props) => {
     description: '',
     options: [
       {
-        name: '',
+        name: 'Default',
         choices: [
           {
-            name: '',
+            name: 'Default',
             price: '',
           },
         ],
@@ -58,10 +68,10 @@ const AddFood = (props) => {
     description: '',
     options: [
       {
-        name: '',
+        name: 'Default',
         choices: [
           {
-            name: '',
+            name: 'Default',
             price: '',
           },
         ],
@@ -77,6 +87,14 @@ const AddFood = (props) => {
 
   const [catagoryAPIState, setCatagoryAPIState] = useState({
     catagories: [],
+  })
+
+  const [foodsState, setFoodsState] = useState({
+    foods: [],
+  })
+
+  const [indexState, setIndexState] = useState({
+    index: -1,
   })
 
   const verifyAddFoodState = () => {
@@ -284,8 +302,24 @@ const AddFood = (props) => {
       }
       food.options[i].choices.sort((a, b) => (a.price > b.price ? 1 : -1))
     }
-    createFood(food)
-    setAddFoodState({ ...emptyState })
+    if (indexState.index < 0) {
+      createFood(food)
+        .then(() => {
+          getFood().then(({ data: foods }) => {
+            setFoodsState({ foods: foods })
+            setAddFoodState({ ...emptyState })
+          })
+        })
+        .catch((err) => console.error(err))
+    } else {
+      updateFood(food._id, food)
+        .then(() => {
+          getFood().then(({ data: foods }) => {
+            setFoodsState({ foods: foods })
+          })
+        })
+        .catch((err) => console.error(err))
+    }
   }
 
   const handleCreateCatagory = (event) => {
@@ -295,7 +329,7 @@ const AddFood = (props) => {
         setAddCatagoryState({ name: '' })
         setCatagoryAPIState({ catagories: catagories })
       })
-      .catch((err) => console.log(err))
+      .catch((err) => console.error(err))
   }
 
   const handleDeleteCatagory = (id) => {
@@ -313,138 +347,23 @@ const AddFood = (props) => {
       .then(({ data: catagories }) => {
         setCatagoryAPIState({ catagories: catagories })
       })
-      .catch((err) => console.log(err))
+      .catch((err) => console.error(err))
+    getFood()
+      .then(({ data: foods }) => {
+        setFoodsState({ foods: foods })
+      })
+      .catch((err) => console.error(err))
   }, [])
 
-  return (
-    <Card className={classes.root}>
-      <Typography>Add Food</Typography>
-      <CardContent>
-        <Card>
-          <CardContent>
-            <TextField
-              id="outlined-basic"
-              label="Name"
-              name="name"
-              variant="outlined"
-              onChange={handleInputChange}
-              value={addFoodState.name}
-            />
-          </CardContent>
-        </Card>
-        <Card>
-          <CardContent>
-            <TextField
-              id="outlined-basic"
-              label="Image"
-              name="image"
-              variant="outlined"
-              onChange={handleInputChange}
-              value={addFoodState.image}
-            />
-          </CardContent>
-        </Card>
-        <Card>
-          <CardContent>
-            <Typography>Options</Typography>
-            {addFoodState.options.map((option, i) => (
-              <Card>
-                <CardContent>
-                  <TextField
-                    id="outlined-basic"
-                    label="Option"
-                    name="name"
-                    variant="outlined"
-                    value={addFoodState.options[i].name}
-                    onChange={(event) => handleOptionChange(event, i)}
-                  />
-                  {option.choices.map((choice, j) => (
-                    <Card>
-                      <CardContent>
-                        <TextField
-                          id="outlined-basic"
-                          label="Choice"
-                          name="name"
-                          variant="outlined"
-                          value={addFoodState.options[i].choices[j].name}
-                          onChange={(event) => handleChoiceChange(event, i, j)}
-                        />
-                        <TextField
-                          id="outlined-basic"
-                          label="Price"
-                          name="price"
-                          variant="outlined"
-                          value={addFoodState.options[i].choices[j].price}
-                          onChange={(event) =>
-                            handleChoicePriceChange(event, i, j)
-                          }
-                        />
-                      </CardContent>
-                      {renderRemoveChoiceButton(i, j)}
-                    </Card>
-                  ))}
-                  <Button
-                    size="small"
-                    color="primary"
-                    variant="contained"
-                    onClick={() => handleAddChoice(i)}
-                  >
-                    Add Choice
-                  </Button>
-                </CardContent>
-                {renderRemoveOptionButton(i)}
-              </Card>
-            ))}
-            <Button
-              size="small"
-              color="primary"
-              variant="contained"
-              onClick={handleAddOption}
-            >
-              Add Option
-            </Button>
-          </CardContent>
-        </Card>
+  const handleSelectFood = (index) => {
+    let food = JSON.parse(JSON.stringify(foodsState.foods[index]))
+    setAddFoodState({ ...food })
+    setIndexState({ index: index })
+  }
 
-        <Card>
-          <CardContent>
-            <InputLabel id="catagory-label">Catagory</InputLabel>
-            <Select
-              labelId="catagory-label"
-              id="demo-simple-select"
-              name="catagory"
-              value={addFoodState.catagory}
-              onChange={handleInputChange}
-            >
-              {catagoryAPIState.catagories.map((catagory) => (
-                <MenuItem value={catagory.name}>{catagory.name}</MenuItem>
-              ))}
-            </Select>
-          </CardContent>
-        </Card>
-
-        <Card>
-          <CardContent>
-            <TextField
-              id="outlined-basic"
-              label="Description"
-              name="description"
-              variant="outlined"
-              onChange={handleInputChange}
-              value={addFoodState.description}
-            />
-          </CardContent>
-        </Card>
-        <Card>
-          <CardContent>
-            <Typography>
-              Lowest Cost: ${numberToMoney(addFoodState.lowestPrice)}
-            </Typography>
-            <Typography>
-              Highest Cost: ${numberToMoney(addFoodState.highestPrice)}
-            </Typography>
-          </CardContent>
-        </Card>
+  const renderAddButton = () => {
+    if (indexState.index < 0) {
+      return (
         <Button
           size="small"
           color="primary"
@@ -454,43 +373,280 @@ const AddFood = (props) => {
         >
           Add Food To Menu
         </Button>
-      </CardContent>
-      <Typography>Add Catagory</Typography>
-      <CardContent>
-        <TextField
-          id="outlined-basic"
-          label="Name"
-          name="name"
-          variant="outlined"
-          onChange={handleChangeCatagory}
-          value={addCatagoryState.name}
-        />
+      )
+    } else {
+      return (
         <Button
-          variant="contained"
           size="small"
           color="primary"
-          onClick={handleCreateCatagory}
-          disabled={!isAddCatagoryEnabled}
+          variant="contained"
+          onClick={handleCreateFood}
+          disabled={!isAddFoodEnabled}
         >
-          Add
+          {`Edit Food #${indexState.index}`}
         </Button>
-        {catagoryAPIState.catagories.map((catagory) => (
+      )
+    }
+  }
+
+  const handleDeleteFood = (index) => {
+    let food = foodsState.foods[index]
+    deleteFood(food._id)
+      .then(() => {
+        getFood().then(({ data: foods }) => {
+          let i = indexState.index
+          if (i >= index) {
+            i = i - 1
+          }
+          setIndexState({ index: i })
+          setFoodsState({ foods: foods })
+          setAddFoodState({ ...emptyState })
+        })
+      })
+      .catch((err) => console.error(err))
+  }
+  return (
+    <Grid container xs={12}>
+      <Grid xs={3}>
+        <Button
+          size="small"
+          color="primary"
+          variant="contained"
+          onClick={() => {
+            setIndexState({ index: -1 })
+            setAddFoodState({ ...emptyState })
+          }}
+        >
+          Add New Food
+        </Button>
+        {foodsState.foods.map((food, i) => (
           <Card>
-            <CardContent>
-              <Typography>{catagory.name}</Typography>
+            <CardActionArea onClick={() => handleSelectFood(i)}>
+              <img
+                src={food.image}
+                alt={food.name}
+                width={'100%'}
+                height={'90px'}
+              />
               <Button
-                variant="contained"
                 size="small"
                 color="secondary"
-                onClick={() => handleDeleteCatagory(catagory._id)}
+                variant="contained"
+                style={{ width: '100%' }}
+                onClick={(event) => {
+                  event.stopPropagation()
+                  handleDeleteFood(i)
+                }}
               >
                 Remove
               </Button>
-            </CardContent>
+              <CardContent>
+                <Typography>ID: {food._id}</Typography>
+                <Typography>Name: {food.name}</Typography>
+                <Typography>Catagory:{food.catagory}</Typography>
+                {food.options.map((option) => (
+                  <Card>
+                    <CardContent>
+                      <Typography>Option: {option.name}</Typography>
+                      {option.choices.map((choice) => (
+                        <Card>
+                          <Typography>Choice: {choice.name}</Typography>
+                          <Typography>
+                            Price: ${numberToMoney(choice.price)}
+                          </Typography>
+                        </Card>
+                      ))}
+                    </CardContent>
+                  </Card>
+                ))}
+                <Typography>
+                  Lowest Price: ${numberToMoney(food.lowestPrice)}
+                </Typography>
+                <Typography>
+                  Highest Price: ${numberToMoney(food.highestPrice)}
+                </Typography>
+                <Typography>Description:</Typography>
+                <Card>
+                  <CardContent>
+                    <Typography>{food.description}</Typography>
+                  </CardContent>
+                </Card>
+              </CardContent>
+            </CardActionArea>
           </Card>
         ))}
-      </CardContent>
-    </Card>
+      </Grid>
+      <Grid xs={9}>
+        <Card className={classes.root}>
+          <Typography>Add Food</Typography>
+          <CardContent>
+            <Card>
+              <CardContent>
+                <TextField
+                  id="outlined-basic"
+                  label="Name"
+                  name="name"
+                  variant="outlined"
+                  onChange={handleInputChange}
+                  value={addFoodState.name}
+                />
+              </CardContent>
+            </Card>
+            <Card>
+              <CardContent>
+                <TextField
+                  id="outlined-basic"
+                  label="Image"
+                  name="image"
+                  variant="outlined"
+                  onChange={handleInputChange}
+                  value={addFoodState.image}
+                />
+              </CardContent>
+            </Card>
+            <Card>
+              <CardContent>
+                <Typography>Options</Typography>
+                {addFoodState.options.map((option, i) => (
+                  <CardContent>
+                    <CardContent>
+                      <TextField
+                        id="outlined-basic"
+                        label="Option"
+                        name="name"
+                        variant="outlined"
+                        value={addFoodState.options[i].name}
+                        onChange={(event) => handleOptionChange(event, i)}
+                      />
+                      {option.choices.map((choice, j) => (
+                        <CardContent>
+                          <CardContent>
+                            <TextField
+                              id="outlined-basic"
+                              label="Choice"
+                              name="name"
+                              variant="outlined"
+                              value={addFoodState.options[i].choices[j].name}
+                              onChange={(event) =>
+                                handleChoiceChange(event, i, j)
+                              }
+                            />
+                            <TextField
+                              id="outlined-basic"
+                              label="Price"
+                              name="price"
+                              variant="outlined"
+                              value={addFoodState.options[i].choices[j].price}
+                              onChange={(event) =>
+                                handleChoicePriceChange(event, i, j)
+                              }
+                            />
+                          </CardContent>
+                          {renderRemoveChoiceButton(i, j)}
+                        </CardContent>
+                      ))}
+                      <Button
+                        size="small"
+                        color="primary"
+                        variant="contained"
+                        onClick={() => handleAddChoice(i)}
+                      >
+                        Add Choice
+                      </Button>
+                    </CardContent>
+                    {renderRemoveOptionButton(i)}
+                  </CardContent>
+                ))}
+                <Button
+                  size="small"
+                  color="primary"
+                  variant="contained"
+                  onClick={handleAddOption}
+                >
+                  Add Option
+                </Button>
+              </CardContent>
+            </Card>
+
+            <Card>
+              <CardContent>
+                <InputLabel id="catagory-label">Catagory</InputLabel>
+                <Select
+                  labelId="catagory-label"
+                  id="demo-simple-select"
+                  name="catagory"
+                  value={addFoodState.catagory}
+                  onChange={handleInputChange}
+                >
+                  {catagoryAPIState.catagories.map((catagory) => (
+                    <MenuItem value={catagory.name}>{catagory.name}</MenuItem>
+                  ))}
+                </Select>
+              </CardContent>
+            </Card>
+
+            <CardContent>
+              <CardContent>
+                <TextField
+                  id="outlined-basic"
+                  label="Description"
+                  name="description"
+                  variant="outlined"
+                  onChange={handleInputChange}
+                  value={addFoodState.description}
+                />
+              </CardContent>
+            </CardContent>
+            <Card>
+              <CardContent>
+                <Typography>
+                  Lowest Cost: ${numberToMoney(addFoodState.lowestPrice)}
+                </Typography>
+                <Typography>
+                  Highest Cost: ${numberToMoney(addFoodState.highestPrice)}
+                </Typography>
+              </CardContent>
+            </Card>
+            {renderAddButton()}
+          </CardContent>
+          <Typography>Add Catagory</Typography>
+          <CardContent>
+            <TextField
+              id="outlined-basic"
+              label="Name"
+              name="name"
+              variant="outlined"
+              onChange={handleChangeCatagory}
+              value={addCatagoryState.name}
+            />
+            <Button
+              variant="contained"
+              size="small"
+              color="primary"
+              onClick={handleCreateCatagory}
+              disabled={!isAddCatagoryEnabled}
+            >
+              Add
+            </Button>
+            {catagoryAPIState.catagories.map((catagory) => (
+              <Card>
+                <CardContent>
+                  <Typography>{catagory.name}</Typography>
+                  <Button
+                    variant="contained"
+                    size="small"
+                    color="secondary"
+                    onClick={() => handleDeleteCatagory(catagory._id)}
+                  >
+                    Remove
+                  </Button>
+                </CardContent>
+              </Card>
+            ))}
+          </CardContent>
+        </Card>
+      </Grid>
+    </Grid>
   )
 }
 
